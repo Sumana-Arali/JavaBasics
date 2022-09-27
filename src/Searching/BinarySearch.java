865aed2f656ce8103246d3bc9a74a24369100cef
Sumana-Arali <sumanabasavarajarali@gmail.com>
2022-09-27 15:18:23 +0530

    Arrays

--- src/Searching/BinarySearch.java
+++ src/Searching/BinarySearch.java
@@ -3,6 +3,34 @@ import java.util.*;
 
 public class BinarySearch {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the array size: ");
+        int n=sc.nextInt();
+        System.out.println("Enter the arrays elemsnts: ");
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Enter the element to be found: ");
+        int key=sc.nextInt();
 
+        //Binary Search
+        int first=0,last=n-1,mid;
+        mid=(first+last)/2;
+        while( first <= last ){
+            if ( arr[mid] < key ){
+                first = mid + 1;
+            }else if ( arr[mid] == key ){
+                System.out.println("Element is found at index: " + mid);
+                break;
+            }else{
+                last = mid - 1;
+            }
+            mid = (first + last)/2;
+        }
+        if ( first > last ){
+            System.out.println("Element is not found!");
+        }
     }
 }
