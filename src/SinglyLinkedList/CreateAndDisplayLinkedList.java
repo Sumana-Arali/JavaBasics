7baa939a3f04a47062e7365a64cf5eaadeb864bb
Sumana-Arali <sumanabasavarajarali@gmail.com>
2022-10-15 10:19:02 +0530

    Arrays

---
+++ src/SinglyLinkedList/CreateAndDisplayLinkedList.java
@@ -0,0 +1,12 @@
+package SinglyLinkedList;
+import java.util.*;
+public class CreateAndDisplayLinkedList {
+       public static void main(String[] args) {
+            LinkedList<String> name=new LinkedList<>();
+            name.add("My");
+            name.add("name");
+            name.add("is");
+            name.add("Suman");
+           System.out.println(name);
+    }
+}
